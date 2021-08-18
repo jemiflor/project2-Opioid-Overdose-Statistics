6055e13bda9274c6ddefb30da819ada19bb16882
Jemima <jemiflor@gmail.com>
2021-08-17 19:00:34 -0500

    Cleaned data file

--- Data/app.py
+++ Data/app.py
@@ -1,7 +1,34 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Aug 14 05:21:43 2021
-
-@author: jemif
+@author: Bootcamp - Project2-Opioid-Overdose-statistics-team
+@module: etl-import excel to mongodb
 """
 
+import pandas as pd
+import pymongo
+import json
+
+#################################################
+# Initialize PyMongo to work with MongoDBs
+#################################################
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+
+#################################################
+# Define database and collection
+#################################################
+mongo_Db = client.opioid_crisis_db
+opioid_stats_collection = mongo_Db["opioidstats"]
+
+
+#################################################
+# Import data file to mongodb
+#################################################
+filename = "opioid_crisis_stats.csv"
+opioidCrisisData = pd.read_csv(filename, usecols=['State', 'Year', 'Month', 'Indicator', 'Data Value']);
+opioidCrisisJson = json.loads(opioidCrisisData.to_json(orient='records'));
+
+# Remove any old collection
+opioid_stats_collection.remove()
+opioid_stats_collection.insert(opioidCrisisJson)
